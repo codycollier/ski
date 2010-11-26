12cf7ade0b35dd7ca5f9247fcd0ec83a730aef40
Cody Collier <cody@telnet.org>
2010-11-26 22:52:41 +0000

    doc - adding licensing information

--- skier.py
+++ skier.py
@@ -10,6 +10,26 @@ keys:
 
 """
 
+""" 
+Copyright 2003-2004 Cody Collier
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+__author__ = "Cody Collier (cody@telnet.org)"
+__copyright__ = "Copyright 2003-2004 Cody Collier"
+__license__ = "Apache License Version 2.0"
 __history__ = """
 0.1 -> 05/15/2003
     -> First incarnation.  Execution.
@@ -43,17 +63,11 @@ ToDo:
 
 """
 
-##############################################################################
-# Import statements
-#
 import sys
 import random
 import time
 import tty, termios, select
 
-##############################################################################
-# Functions
-#
 
 #-----------------------------------------------------------------------------
 # calculation functions
